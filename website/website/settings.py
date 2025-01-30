d45c0586090d9d9337d707a36c7d50ff0b63965b
Umesh <umeshtamang129@gmail.com>
2025-01-30 10:07:45 +0545

    connect mysql database

--- website/website/settings.py
+++ website/website/settings.py
@@ -72,10 +72,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# mysql database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'managementsystemwebsite_database',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
